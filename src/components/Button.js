ccf8cc0a1fd0407b1d453925a9571e080f4c18c7
Stephen Kovolski <s.kovolski@gmail.com>
2023-05-12 23:51:48 -0400

    button

--- src/components/Button.js
+++ src/components/Button.js
@@ -4,7 +4,9 @@ import React from "react";
 function Button() {
     const refresh = () => window.location.reload(true)
 return(
+    <div>
     <button onClick={refresh}>Refresh</button>
+    </div>
     )
 }
 
